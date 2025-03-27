a608861fb84dd0ce940da8b8de65e53cba71e27e
Kevin Sandberg <Kevinsandb3607@student.lvusd.org>
2025-03-27 11:37:00 -0700

    Realized I am a big dumb idiot and thought that the browse was a herbivore when the paper referedd to plants

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,1 +1,9 @@
+from math import sin, pi
+
 time = 0
+
+
+def periodic_func_summer(seasonality_herb_growth):
+    global time
+    return 1 + seasonality_herb_growth * sin(time*2*pi)
+
--- Plants.py
+++ Plants.py
@@ -1,6 +1,6 @@
 from entities import Entity
 #Makes herbivore class
-class Herbivore(Entity):
+class Plants(Entity):
     def __init__(self, density, growth_rate, equilibrium_density, max_rate_removal, half_saturation, cond_growth, cond_consumption):
         Entity.__init__(self, density)
         self.growth_rate = growth_rate
@@ -9,3 +9,7 @@ class Herbivore(Entity):
         self.half_saturation = half_saturation
         self.cond_growth = cond_growth
         self.cond_consumption = cond_consumption
+
+    # gets the growth rate accounting for time differences, kg / (km^2 * yr)
+    def find_effective_growth(self, browse_density, time):
+
