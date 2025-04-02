a2d5fd07e3fe546b267f5f8bb0804e8bd0315f96
Kevin Sandberg <Kevinsandb3607@student.lvusd.org>
2025-04-02 12:21:07 -0700

    Finished Predator Derivative

--- Predator.py
+++ Predator.py
@@ -1,4 +1,5 @@
 from entities import Entity
+from GlobalVariables import periodic_func_summer
 class Predator(Entity):
     def __init__(self, density, recruitment_rate, max_kill_rate, half_saturation, cond_dep_mortality):
         Entity.__init__(self, density)
@@ -6,4 +7,8 @@ class Predator(Entity):
         self.max_kill_rate = max_kill_rate
         self.half_saturation = half_saturation
         self.cond_dep_mortality = cond_dep_mortality
+
+    def find_predator_derivative(self, prey):
+        growth = self.recruitment_rate*periodic_func_summer()*prey.density*self.density / (self.half_saturation + prey.density)
+        starvation = self.cond_dep_mortality*periodic_func_summer()*prey.density*self.density/(self.half_saturation + prey.density) * self.density
 
